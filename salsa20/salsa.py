de1cf3cb26a0f6c8a55216fd6f40e96fc19cffd4
Danil <danil.nastarchuk@gmail.com>
2022-04-07 23:55:13 +0300

    added main func

--- salsa20/salsa.py
+++ salsa20/salsa.py
@@ -1,9 +1,13 @@
-with open("encrypted.txt") as file:
-    input_text = [bytearray.fromhex(line) for line in file.readlines()]
+import sys
+from typing import TextIO
 
-input_key = bytearray.fromhex("the pangs of dispriz'd love, the law's delay,".encode('utf-8').hex())
-key = [a ^ b for a, b in zip(input_text[2], input_key)]
 
-with open("decrypted.txt", "a") as file:
-    decrypted = [''.join([chr(a ^ b) for a, b in zip(key, line)]) for line in input_text]
-    print(decrypted)
+def main(source: TextIO, dest: TextIO, key: bytearray):
+    input_text = [bytearray.fromhex(line) for line in source.readlines()]
+    source_key = [a ^ b for a, b in zip(input_text[2], key)]
+    decrypted = ''.join([''.join([chr(a ^ b) for a, b in zip(source_key, line)]) for line in input_text])
+    dest.write(decrypted)
+
+
+if __name__ == '__main__':
+    main(open('encrypted.txt'), sys.stdout, bytearray(b"the pangs of dispriz'd love, the law's delay,"))
