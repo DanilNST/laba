e80b89497143e75416fa022d1f475193a6384e46
DanilNST <danil.nastarchuk@gmail.com>
2022-04-08 01:13:31 +0300

    add creating csv with hashes

--- human_password/password_generator.py
+++ human_password/password_generator.py
@@ -1,58 +1,53 @@
-from random import randint, choice
-from string import ascii_letters, digits
+import csv
+import random
 import hashlib
 import bcrypt
-
-
-
-
-def get10M():
-    passwords = []
-    with open('10m-passwords.txt') as file:
-        for p in file.readlines():
-            p = p.strip()
-            passwords.append(p)
-    return passwords
+from string import ascii_letters, digits
 
 
 def generate(num, top_100, top_10M):
     passwords = []
     for _ in range(num):
-        r = randint(1, 100)
-        if r in range(1, 10):
-            passwords.append(choice(top_100))
-        elif r in range(11, 95):
-            passwords.append(choice(top_10M))
+        r = random.randint(1, 100)
+        if r <= 10:
+            passwords.append(random.choice(top_100))
+        elif r <= 95:
+            passwords.append(random.choice(top_10M))
         else:
-            passwords.append(''.join([choice(ascii_letters+digits) for _ in range(randint(8, 16))]))
+            password_symbols = ascii_letters + digits
+            passwords.append(''.join(random.choices(password_symbols, k=random.randint(8, 16))))
     return passwords
 
 
 def pass_md5(password):
-    return hashlib.md5(bytes(password, encoding="ascii")).hexdigest()
+    return hashlib.md5(bytes(password, encoding='ascii')).hexdigest()
 
 
 def pass_bcrypt(password):
     salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(bytes(password, encoding="ascii"), salt).decode()
-    print(hashed)
+    hashed = bcrypt.hashpw(bytes(password, encoding='ascii'), salt).decode()
     return salt, hashed
 
 
 def main():
-    with open('100_passwords.txt') as file:
+    with open('100-passwords.txt') as file:
         top_100 = [password.strip() for password in file.readlines()]
 
     with open('10m-passwords.txt') as file:
         top_10m = [password.strip() for password in file.readlines()]
 
     passwords = generate(100, top_100, top_10m)
-    print('\n'.join(passwords))
-
     with open('100k.txt', 'w') as file:
-        for passwords in passwords:
-            file.write(pass_md5(passwords))
+        for password in passwords:
+            file.write(pass_md5(password))
             file.write('\n')
+
+    with open('10k_hash.csv', 'w') as csv_file:
+        csv_table = csv.writer(csv_file)
+        csv_table.writerow(['hash', 'salt'])
+        for password in passwords:
+            salt, hashed = pass_bcrypt(password)
+            csv_table.writerow([hashed, salt.decode()])
 
 
 if __name__ == '__main__':
