3c3f2e9ef4476b7b61559724944e2c456436c58f
DanilNST <danil.nastarchuk@gmail.com>
2022-04-08 01:30:11 +0300

    Add arguments for script

--- human_password/password_generator.py
+++ human_password/password_generator.py
@@ -1,54 +1,66 @@
+import argparse
 import csv
 import random
 import hashlib
+from typing import TextIO, Tuple
+
 import bcrypt
 from string import ascii_letters, digits
 
 
-def generate(num, top_100, top_10M):
+def generate(num, top_100, top_10m):
     passwords = []
     for _ in range(num):
         r = random.randint(1, 100)
         if r <= 10:
             passwords.append(random.choice(top_100))
         elif r <= 95:
-            passwords.append(random.choice(top_10M))
+            passwords.append(random.choice(top_10m))
         else:
             password_symbols = ascii_letters + digits
             passwords.append(''.join(random.choices(password_symbols, k=random.randint(8, 16))))
     return passwords
 
 
-def pass_md5(password):
-    return hashlib.md5(bytes(password, encoding='ascii')).hexdigest()
+def pass_md5(password: str) -> Tuple[str]:
+    return hashlib.md5(bytes(password, encoding='ascii')).hexdigest(),
 
 
-def pass_bcrypt(password):
+def pass_bcrypt(password: str) -> Tuple[str, str]:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(bytes(password, encoding='ascii'), salt).decode()
-    return salt, hashed
+    return salt.decode(), hashed
 
 
-def main():
-    with open('100-passwords.txt') as file:
-        top_100 = [password.strip() for password in file.readlines()]
+def sha_1(password: str) -> Tuple[str, str]:
+    salt = bcrypt.gensalt().decode()
+    return salt, hashlib.sha1(bytes(password+salt, encoding='ascii')).hexdigest()
 
-    with open('10m-passwords.txt') as file:
-        top_10m = [password.strip() for password in file.readlines()]
 
-    passwords = generate(100, top_100, top_10m)
-    with open('100k.txt', 'w') as file:
-        for password in passwords:
-            file.write(pass_md5(password))
-            file.write('\n')
+def main(num: int, top_100: TextIO, top_10m: TextIO, output: TextIO, algorithm: str):
+    algorithms = {'md5': pass_md5, 'bcrypt': pass_bcrypt, 'sha-1': sha_1}
 
-    with open('10k_hash.csv', 'w') as csv_file:
-        csv_table = csv.writer(csv_file)
-        csv_table.writerow(['hash', 'salt'])
-        for password in passwords:
-            salt, hashed = pass_bcrypt(password)
-            csv_table.writerow([hashed, salt.decode()])
+    top_100_passwords = [password.strip() for password in top_100.readlines()]
+    top_10m_passwords = [password.strip() for password in top_10m.readlines()]
+
+    passwords = generate(num, top_100_passwords, top_10m_passwords)
+
+    csv_table = csv.writer(output)
+    if algorithm in ['sha-1', 'bcrypt']:
+        csv_table.writerow(['salt', 'hash'])
+    else:
+        csv_table.writerow(['hash'])
+
+    for password in passwords:
+        csv_table.writerow(algorithms[algorithm](password))
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--num', type=int, default=100)
+    parser.add_argument('-t', '--top_100', type=argparse.FileType('r'), default=open('100-passwords.txt'))
+    parser.add_argument('-m', '--top_10M', type=argparse.FileType('r'), default=open('10m-passwords.txt'))
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=open('10k_hash.csv', 'w'))
+    parser.add_argument('--algorithm', choices=['md5', 'sha-1', 'bcrypt'], default='md5')
+    _args = parser.parse_args()
+    main(_args.num, _args.top_100, _args.top_10M, _args.output, _args.algorithm)
