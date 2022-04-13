fdacad5624ea5490bf4d5ff4bbd6970c7bc124ba
DanilNST <danil.nastarchuk@gmail.com>
2022-04-13 22:49:56 +0300

    finish first step with bas64

---
+++ intro/first_binary_to_text.py
@@ -0,0 +1,12 @@
+import base64
+
+with open('sources/binary.txt') as f:
+    data = f.read()
+
+string = ''
+for byte_num in range(len(data)//8):
+    byte = data[byte_num*8:(byte_num+1)*8]
+    byte_int = int(byte, 2)
+    string += chr(byte_int)
+
+print(base64.b64decode(string).decode())
