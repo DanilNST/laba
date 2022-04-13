c10d46bde823e31ec7a5ae14d64a277a15155c4b
DanilNST <danil.nastarchuk@gmail.com>
2022-04-14 00:20:18 +0300

    add hack xor crypto

---
+++ intro/second_xor_text.py
@@ -0,0 +1,14 @@
+import binascii
+
+dictionary = str.maketrans({'\x00': ' ', '\x0e': '\n', 'Â': '', '': '', '½': "'", '¼': "'"})
+
+with open('sources/xor_secret.txt', 'r') as f:
+    data_lines = f.readlines()[0:1]
+
+for data_line in data_lines:
+    data_line = data_line.strip()
+    data_line = binascii.unhexlify(data_line)
+    new_data = ''
+    for byte in data_line:
+        new_data += chr(byte ^ 23)
+    print(new_data.translate(dictionary))
---
+++ intro/third_key_hack.py
@@ -0,0 +1,26 @@
+import base64
+import string
+
+SYMBOLS = string.ascii_letters + ',. '
+
+
+with open('sources/xor_secret.txt', 'r') as f:
+    encrypted = f.readlines()[1].strip()
+
+fault = 2
+encrypted = base64.b64decode(encrypted)
+
+possible_keys = []
+
+local_max = 0
+for i in range(2**8-1):
+    count = sum(chr(item ^ i) in SYMBOLS for item in encrypted)
+    local_max = max(count, local_max)
+
+for i in range(2**8-1):
+    count = sum(chr(item ^ i) in SYMBOLS for item in encrypted)
+    if count >= local_max - fault:
+        possible_keys.append(chr(i))
+
+print('possible keys:', ','.join(possible_keys))
+
